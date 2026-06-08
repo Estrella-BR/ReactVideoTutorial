0e7ed3c0e68649a52d66236dd01ba1a5e36df6e0
Estrella Blanco <estrellablanro@gmail.com>
2026-06-08 13:06:35 +0200

    Paso 40: Colocar estilos de CSS

--- 02-first-steps/src/MyAwesomeApp.tsx
+++ 02-first-steps/src/MyAwesomeApp.tsx
@@ -1,8 +1,53 @@
-export function MyAwesomeApp() {
+import type { CSSProperties } from 'react';
+
+const firstName = 'Estrella';
+const lastName = 'Blanco';
+
+const favoriteGames = ['The Legend of Zelda', 'Super Mario Bros', 'Minecraft'];
+const isActive = true;
+
+const address = {
+    street: '123 Main St',
+    city: 'Anytown',
+    country: 'USA'
+}
+
+const myStyle: CSSProperties = {
+    backgroundColor: '#fafafa',
+    borderRadius: 8,
+    padding: 16,
+    marginTop: 16,
+
+}
+//Es mejor tener las componentes fuera para que no se estén ejecutando todo el rato
+
+export const MyAwesomeApp = () => {
+
+
+
+    return (
+        <>
+            <h1>{firstName}</h1>
+            <h3>{lastName}</h3>
+
+            <p>{favoriteGames.join(', ')}</p>
+            <h1>{isActive ? 'Active' : 'Inactive'}</h1>
+            <p>{2 + 2}</p>
+
+            <p
+                style={myStyle}
+            > {JSON.stringify(address)}</p>
+        </>
+    )
+}
+
+
+
+/* export function MyAwesomeApp() {
     return (
         <>
             <h1>Estrella</h1>
             <h3>Blanco</h3> 
         </>
     )
-}
+} */
